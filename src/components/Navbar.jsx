0a2170e2fa9a0005cf4d5698ca33f763cf31a61f
Riya <riyagarg2309@gmail.com>
2024-04-06 17:49:25 +0530

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+	return (
+		<nav className='flex justify-around bg-indigo-900 text-white py-2'>
+			<div className="logo">
+				<span className='font-bold text-xl mx-8'>ToDo List App</span>
+			</div>
+
+		</nav>
+	)
+}
+
+export default Navbar
